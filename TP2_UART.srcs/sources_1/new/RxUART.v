3370e54ede4b6f32fc58393eb5900fb5f5e974cc
Stefano <stefano.itr.unc@gmail.com>
2023-09-21 16:43:56 -0300

    ALU y baud Rate generator terminado, falta RX, TX y la interface

---
+++ TP2_UART.srcs/sources_1/new/RxUART.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/21/2023 04:33:59 PM
+// Design Name: 
+// Module Name: RxUART
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RxUART(
+
+    );
+endmodule
